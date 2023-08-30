c520f61d1a644bbe3fd5010050aad8a7d777d528
MTKMAMA <143503756+MTKMAMA@users.noreply.github.com>
2023-08-30 07:24:51 +0000

    task 2

--- Harmonious/Harmonious.java
+++ Harmonious/Harmonious.java
@@ -1,6 +1,36 @@
 package Harmonious;
 
 public class Harmonious {
-    
-    
+    int i = 0 ;
+    int min= Integer.MAX_VALUE ;
+    int max= Integer.MIN_VALUE ; 
+      while(i<nums.length)
+  {
+         if(min>nums[i])
+         {
+             min1=i;
+             min=nums[i];
+         }
+         if(max<=nums[i])
+         {
+             max1=i;
+             max=nums[i];
+         }
+         if((max-min)>1)
+         {
+             min1++;
+             min=nums[min1];
+         }
+         else if((max-min)<1)
+         {
+          i++;
+         }
+         else{
+             lhs=Math.max(lhs,(max1-min1)+1);
+             i++;
+              }
+           
+  }
+  return lhs<0 ? 0 : lhs;
+
 }
