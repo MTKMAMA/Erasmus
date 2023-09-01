4b2d5c951372d5c12989bd627b2a8edc2cc33bcb
MTKMAMA <143503756+MTKMAMA@users.noreply.github.com>
2023-09-01 15:36:26 +0300

    Add files via upload

---
+++ NatureReseveSimulationSolution/src/naturereservesimulation/Animals.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package naturereservesimulation;
+
+/**
+ *
+ * @author Erasmus3
+ */
+public class Animals {
+    public void pelo() {
+    }
+    
+}
